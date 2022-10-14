deaa7fc272aab3afe4c0c10ee8f21cb097caec69
Maorio <915749867@qq.com>
2022-10-14 15:58:13 +0800

    edit

--- layers/fea2embed_layer.py
+++ layers/fea2embed_layer.py
@@ -3,9 +3,6 @@ import torch.nn as nn
 
 
 class fea2embed_head(nn.Module):
-    """
-    简单的一个MLP，由结点的features预测该结点经过全图GAT的输出embedding
-    """
     def __init__(self, in_size, out_size):
         super().__init__()
         self.leakyrelu = nn.LeakyReLU()
@@ -14,10 +11,6 @@ class fea2embed_head(nn.Module):
         self.conv2 = nn.Conv1d(hid_dim, out_size, 1)
 
     def forward(self, h):
-        """
-        输入h是一个矩阵[1, in_size, num_nodes]
-        输出[1, out_size, num_nodes]
-        """
         out = self.leakyrelu(self.conv1(h))
         out = self.leakyrelu(self.conv2(out))
         return out
